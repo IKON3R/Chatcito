b69d0e1dc510cbaedf43347ff1b249647b7fa0f0
IKON3R <operezz2100@alumno.ipn.mx>
2023-11-29 09:00:16 -0600

    comentarios

--- app.js
+++ app.js
@@ -1,43 +1,64 @@
+// Importación de módulos necesarios
 const express = require('express');
 const bodyParser = require('body-parser');
+
+// Creación de la aplicación Express
 var app = require('express')();
+// Creación del servidor HTTPS con Express
 var server = require('https').Server(app);
+// Inicialización de Socket.IO en el servidor
 var io = require('socket.io')(server);
 
+// Arreglo para almacenar los clientes conectados
 var clientes = [];
 
+// Configuración de la aplicación Express
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+// Configuración del servidor para escuchar en el puerto 443
 server.listen(443, '0.0.0.0', () => console.log('Servidor iniciado en 443'));
 
+// Ruta para la página principal
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
+// Ruta para la página de chat con un nombre de usuario específico
 app.get('/chat/:username', function (req, res) {
   res.sendFile(__dirname + '/public/chat.html');
 });
 
+// Endpoint para el inicio de sesión en el chat
 app.post('/login', function (req, res) {
   let username = req.body.username;
   let id = req.body.id;
-  clientes.push({id: id, username: username});
-  io.emit('socket_conectado', {id: id, username: username});
+  // Almacenamiento del usuario conectado en el arreglo de clientes
+  clientes.push({ id: id, username: username });
+  // Emisión de evento 'socket_conectado' a todos los clientes conectados
+  io.emit('socket_conectado', { id: id, username: username });
+  // Devolución de la lista actualizada de clientes al cliente que inició sesión
   return res.json(clientes);
 });
 
+// Endpoint para enviar mensajes en el chat
 app.post('/send', function (req, res) {
   let username = req.body.username;
   let id = req.body.id;
   let msg = req.body.text;
-  io.emit('mensaje', {id: id, msg: msg, username: username});
-  return res.json({text: 'Mensaje enviado.'});
+  // Emisión de evento 'mensaje' a todos los clientes con el mensaje y el remitente
+  io.emit('mensaje', { id: id, msg: msg, username: username });
+  // Confirmación de que el mensaje fue enviado correctamente
+  return res.json({ text: 'Mensaje enviado.' });
 });
 
+// Manejador de eventos cuando se establece una conexión con Socket.IO
 io.on('connection', socket => {
   console.log('Socket conectado', socket.id);
+  // Manejador de evento cuando un cliente se desconecta
   socket.on('disconnect', () => {
+    // Filtrar y eliminar al cliente que se desconectó del arreglo de clientes
     clientes = clientes.filter(cliente => cliente.id != socket.id);
-    io.emit('socket_desconectado', {texto: 'Socket desconectado.', id: socket.id});
+    // Emisión de evento 'socket_desconectado' a todos los clientes
+    io.emit('socket_desconectado', { texto: 'Socket desconectado.', id: socket.id });
   });
 });
--- public/js/logica.js
+++ public/js/logica.js
@@ -1,28 +1,39 @@
+// Conexión al servidor de Socket.IO en la dirección proporcionada
 var socket = io.connect('https://chat-9buh.onrender.com:443/');
+// Selección del elemento de lista de usuarios y obtención del nombre de usuario desde la URL
 var list = document.querySelector('#lista-users');
 var username = window.location.pathname.replace('/chat/', '');
+// Arreglo para almacenar los clientes conectados
 var clientes = [];
 
+// Función para conectarse al chat
 function conectarChat() {
+  // Obtención del ID del socket y envío de solicitud de login al servidor
   var id = socket.id;
   console.log('id:', socket.id, 'useranme:', username);
   $.post('/login', {username: username, id: id}, function (data) {
     console.log(data);
+    // Almacenamiento de los datos de clientes recibidos del servidor
     clientes = data;
+    // Mostrar mensaje de carga mientras se procesa la lista de usuarios
     list.innerHTML += 'Cargando...';
     var html = '';
+    // Generación de la lista de usuarios conectados
     clientes.forEach(function (cliente) {
       html += '<li>' + cliente.username + '</li>';
     });
     list.innerHTML = html;
+    // Ocultar el indicador de carga una vez que se completa la operación
     $('.loader').hide();
   });
 }
 
+// Función para enviar mensajes al presionar Enter
 function enviarMensaje(e) {
   if (e.which != 13) return;
   var msg = document.querySelector('#input').value;
   if (msg.length <= 0) return;
+  // Envío del mensaje al servidor
   $.post('/send', {
     text: msg,
     username: username,
@@ -32,11 +43,14 @@ function enviarMensaje(e) {
   });
 }
 
+// Evento para recibir mensajes del servidor y mostrarlos en el chat
 socket.on('mensaje', function (data) {
+  // Sanitización de los datos recibidos para evitar posibles vulnerabilidades
   data.username = data.username.replace('</', '');
   var sanitized = data.msg.replace('</', '');
   sanitized = sanitized.replace('>', '');
   sanitized = sanitized.replace('<', '');
+  // Comprobación y generación de mensaje local o remoto
   if (data.id == socket.id) {
     var msj = `
     <div class="local-message">
@@ -54,27 +68,35 @@ socket.on('mensaje', function (data) {
     `;
     document.querySelector('.mensajes-container').innerHTML += msj;
   }
-})
+});
 
+// Evento cuando un socket se desconecta
 socket.on('socket_desconectado', function (data) {
   console.log(data);
+  // Filtrar el cliente desconectado del arreglo de clientes
   clientes = clientes.filter(function (cliente) {
     console.log(cliente);
     return cliente.id != data.id;
   });
+  // Mostrar mensaje de carga mientras se actualiza la lista de usuarios
   list.innerHTML += 'Cargando...';
   var html = '';
+  // Actualizar la lista de usuarios conectados
   clientes.forEach(function (cliente) {
     html += '<li>' + cliente.username + '</li>';
   });
   list.innerHTML = html;
 });
 
+// Evento cuando un socket se conecta
 socket.on('socket_conectado', function (data) {
   console.log(data);
+  // Agregar el nuevo cliente al arreglo de clientes
   clientes.push(data);
+  // Mostrar mensaje de carga mientras se actualiza la lista de usuarios
   list.innerHTML += 'Cargando...';
   var html = '';
+  // Actualizar la lista de usuarios conectados
   clientes.forEach(function (cliente) {
     html += '<li>' + cliente.username + '</li>';
   });
